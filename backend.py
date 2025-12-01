5b01fa124cad91fb17371353ffdd128841a767d1
lingyi0110 <79823959+LingYi0110@users.noreply.github.com>
2025-12-02 02:21:34 +0800

    先添加两个数据集，写了个能切换的简易后端

---
+++ backend.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+class Backend:
+    def __init__(self):
+        self._backend = np
+        self._type = 'numpy'
+
+    def set_backend(self, backend: str):
+        if backend == 'cupy':
+            try:
+                import cupy as cp
+                self._backend = cp
+                self._type = 'cupy'
+            except ImportError:
+                self._backend = np
+                self._type = 'numpy'
+        elif backend == 'numpy':
+            self._backend = np
+            self._type = 'numpy'
+        else:
+            # 代码健壮性这一块还是要的
+            raise NotImplementedError('Backend {} not implemented'.format(backend))
+
+    def __getattr__(self, name):
+        return getattr(self._backend, name) # 把对应的调用送到指定后端
+
+
+xp = Backend()
+
+def set_backend(backend: str):
+    xp.set_backend(backend)
